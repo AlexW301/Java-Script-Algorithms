e32b424acf5cce854b92598dc24df47c3ad3ed34
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-07-01 07:43:06 -0400

    Update Bubble-Sort.js

--- Bubble-Sort.js
+++ Bubble-Sort.js
@@ -1,9 +1,11 @@
+//Work in Progress
+
 function bubbleSort(array) {
     for(let i = array.length - 1; i > 0; i--) {
         for(let j = 0; j < i; j++) {
             if(array[j] > array[j+1]) {
                 let temp = array[j]
-                array[j] = array=[j+1]
+                array[j] = array[j+1]
                 array[j+1] = temp
             }
         }
