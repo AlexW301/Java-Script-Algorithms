44fe0af9bc0467c12f8372bede0d97bd2d717352
AlexW301 <66179529+AlexW301@users.noreply.github.com>
2021-07-05 09:56:41 -0400

    Update Tree-Traversal.js

--- Tree-Traversal.js
+++ Tree-Traversal.js
@@ -65,6 +65,39 @@ class BST {
     }
     return results;
   }
+
+  DFSPreorder() {
+    let results = [];
+    function traverse(currentNode) {
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
+
+  DFSPostorder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+      results.push(currentNode.value);
+    }
+    traverse(this.root);
+    return results;
+  }
+
+  DFSInorder() {
+    let results = [];
+    function traverse(currentNode) {
+      if (currentNode.left) traverse(currentNode.left);
+      results.push(currentNode.value);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
 
 let myTree = new BST();
